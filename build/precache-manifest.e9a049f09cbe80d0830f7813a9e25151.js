89f93ab5c4687a6e9a9624660bae8ef31badd20b
Sylvester Chenne <sylvesterchenne@Kiwi.hitronhub.home>
2020-06-24 21:50:43 -0700

    readme

---
+++ build/precache-manifest.e9a049f09cbe80d0830f7813a9e25151.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb03ba055a2e1577b47d12b898167cd7",
+    "url": "/React-Meme-Generator/index.html"
+  },
+  {
+    "revision": "0b5ae3368244013d17bc",
+    "url": "/React-Meme-Generator/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "0b8e9b71d17a5e28ce05",
+    "url": "/React-Meme-Generator/static/css/main.2d9dcef7.chunk.css"
+  },
+  {
+    "revision": "0b5ae3368244013d17bc",
+    "url": "/React-Meme-Generator/static/js/2.f20b2d24.chunk.js"
+  },
+  {
+    "revision": "9a9e56582fbb6743dfc4c1baab3b2d6f",
+    "url": "/React-Meme-Generator/static/js/2.f20b2d24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b8e9b71d17a5e28ce05",
+    "url": "/React-Meme-Generator/static/js/main.8bdaa623.chunk.js"
+  },
+  {
+    "revision": "e541d1040d47b4a2602e",
+    "url": "/React-Meme-Generator/static/js/runtime-main.c0203a03.js"
+  }
+]);
